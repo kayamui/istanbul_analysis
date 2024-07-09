48c56665d6ed204a4808921779a5b21ffefa7e7b
kayamui <muhammedkaya65@gmail.com>
2024-07-09 22:28:26 +0300

    frontend_files_removed

--- yazılım/api/common/response.py
+++
@@ -1,7 +0,0 @@
-# api/common/response.py
-def http_response_object(success: bool, message: str, data: dict, code: int):
-    return {
-        "success": success,
-        "message": message,
-        "data": data
-    }, code
